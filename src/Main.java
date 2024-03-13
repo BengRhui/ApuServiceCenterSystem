e50f134460409366f5bd3ed0004fc6f6cbe15fe9
Beh0404 <163319574+Beh0404@users.noreply.github.com>
2024-03-14 01:03:21 +0800

    qwe
    qwe

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,70 @@
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Main {
+import static jdk.internal.org.jline.utils.InfoCmp.Capability.buttons;
+
+public class Main implements ActionListener {
+
+    private static JLabel userLabel;
+    private static JTextField userText;
+    private static JLabel passwordLabel;
+    private static JPasswordField passwordText;
+    private static JButton button;
+    private static JLabel success;
+
+
     public static void main(String[] args) {
 
         JPanel panel = new JPanel();
         JFrame frame = new JFrame();
-        frame.setSize(1250,900);
-        frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1250, 900);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
 
         panel.setLayout(null);
 
-        JLabel userLabel = new JLabel("Email");
+        userLabel = new JLabel("Email");
         userLabel.setBounds(650, 300, 80, 25);
         panel.add(userLabel);
 
-        JTextField userText = new JTextField();
-        userText.setBounds(650,325,250,40);
+        userText = new JTextField();
+        userText.setBounds(650, 325, 250, 40);
         panel.add(userText);
-        Border rotatedBorder = (Border) new RotatedLineBorder(Color.BLACK, 15);
+//        Border rotatedBorder = (Border) new RotatedLineBorder(Color.BLACK, 15);
 
-        JLabel passwordLabel = new JLabel ("Password");
-        passwordLabel.setBounds(650,370,80,25);
+        passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(650, 370, 80, 25);
         panel.add(passwordLabel);
 
-        JPasswordField passwordText = new JPasswordField();
+        passwordText = new JPasswordField();
         passwordText.setBounds(650, 395, 250, 40);
         panel.add(passwordText);
 
+        button = new JButton("Login");
+        button.setBounds(650, 465, 250, 40);
+        button.addActionListener(new Main());
+        panel.add(button);
+
+        success = new JLabel("");
+        success.setBounds(10, 110, 300, 25);
+        panel.add(success);
+
         frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String user = userText.getText();
+        String password = passwordText.getText();
+        System.out.println(user + " " + password);
+
+        if(user.equals("Beh") && password.equals("04")) {
+            success.setText("Login Successful!");
+
+        }
+
     }
 }
