a19e524dd71673f62462ed70d1e0bd537c573e7d
Beh0404 <163319574+Beh0404@users.noreply.github.com>
2024-03-15 01:14:44 +0800

    1
    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import jdk.internal.icu.text.UnicodeSet;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -7,18 +5,18 @@ import java.awt.event.ActionListener;
 
 public class Main implements ActionListener {
 
-    private void initRegister() {
-
-        JPanel Panel = new JPanel();
-        Panel.setBackground(Color.blue);
-        Panel.setBounds(0,0,500,900);
+    public static void main(String[] args){
 
         JPanel panel = new JPanel();
         JFrame frame = new JFrame();
         frame.setSize(1250, 900);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(Panel);
-        frame.add(panel);
+
+
+        JLabel label = new JLabel();
+        label.setIcon(new ImageIcon("C:\\Users\\Admin\\Documents\\GitHub\\ApuServiceCenterSystem\\asset\\background_1.jpg"));
+
+
 
 
         panel.setLayout(null);
@@ -27,9 +25,10 @@ public class Main implements ActionListener {
         userLabel.setBounds(650, 300, 80, 25);
         panel.add(userLabel);
 
-        JTextField txtUser = new JTextField("Email");
-        txtUser.setPrefixIcon(new ImageIcon(getClass().getResource("email_icon.png")));
-        register.add(txtUser, "w 60%");
+        JTextField textField = new JTextField();
+        textField.setBounds(650,325,250,40);
+        panel.add(textField);
+
 //        Border rotatedBorder = (Border) new RotatedLineBorder(Color.BLACK, 15);
 
         JLabel passwordLabel = new JLabel("Password");
@@ -49,10 +48,12 @@ public class Main implements ActionListener {
         success.setBounds(10, 110, 300, 25);
         panel.add(success);
 
-
-
+        frame.add(panel);
+        frame.add(label);
         frame.setVisible(true);
+
     }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
